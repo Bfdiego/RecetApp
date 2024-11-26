715fbccd30947e5e6eb53df35399532b868dcdad
Diego Alba <dayo.alba.p@gmail.com>
2024-11-26 08:49:43 -0400

    Change SDK Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.padresdinamicos"
-    compileSdk = 33
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.padresdinamicos"
         minSdk = 29
-        targetSdk = 33
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
