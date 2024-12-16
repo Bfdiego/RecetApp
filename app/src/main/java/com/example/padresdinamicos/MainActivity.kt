fe8bb27677f2b5c1f7744f2b0f9699328b2f171c
JorgiJust <saenz.jorge.inigues@gmail.com>
2024-12-16 09:14:18 -0400

    Manifest changes, added splash screen, added gif, implemented two new implementations

--- app/src/main/java/com/example/padresdinamicos/MainActivity.kt
+++ app/src/main/java/com/example/padresdinamicos/MainActivity.kt
@@ -2,16 +2,78 @@ package com.example.padresdinamicos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.padresdinamicos.dataclasses.Data
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var datosAdapter: DatosAdapter
+    private val datosList = mutableListOf<Data>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_estilo_formulario)
 
-        val textView: TextView = findViewById(R.id.holamundo)
+        val editTextName = findViewById<EditText>(R.id.name)
+        val editTextTag = findViewById<EditText>(R.id.tag)
+        val editTextCuantity = findViewById<EditText>(R.id.cuantity)
+        val editTextCategory = findViewById<EditText>(R.id.category)
+        val buttonGuardar = findViewById<Button>(R.id.button_guardar)
+        val recyclerView = findViewById<RecyclerView>(R.id.loguardado)
 
-        textView.text = "Hola Adri!"
+
+        datosAdapter = DatosAdapter(datosList)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = datosAdapter
+
+
+        cargarDatos()
+
+
+        buttonGuardar.setOnClickListener {
+            val name = editTextName.text.toString().trim()
+            val tag = editTextTag.text.toString().trim()
+            val quantity = editTextCuantity.text.toString().trim()
+            val category = editTextCategory.text.toString().trim()
+
+            if (name.isNotEmpty() && tag.isNotEmpty() && quantity.isNotEmpty() && category.isNotEmpty()) {
+                guardarDatos(name, tag, quantity, category)
+                datosList.add(Data(name, tag, quantity, category))
+                datosAdapter.notifyItemInserted(datosList.size - 1)
+                limpiarCampos(editTextName, editTextTag, editTextCuantity, editTextCategory)
+            }
+        }
+    }
+
+    private fun guardarDatos(name: String, tag: String, quantity: String, category: String) {
+        val sharedPreferences = getSharedPreferences("activiti_Formulario", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
+        datosList.add(Data(name, tag, quantity, category))
+        val datosJson = Gson().toJson(datosList)
+        editor.putString("datos", datosJson)
+        editor.apply()
+    }
+
+    private fun cargarDatos() {
+        val sharedPreferences = getSharedPreferences("activiti_Formulario", MODE_PRIVATE)
+        val datosJson = sharedPreferences.getString("datos", null)
+        if (!datosJson.isNullOrEmpty()) {
+            val type = object : TypeToken<MutableList<Data>>() {}.type
+            val loadedDatos = Gson().fromJson<MutableList<Data>>(datosJson, type)
+            datosList.addAll(loadedDatos)
+            datosAdapter.notifyDataSetChanged()
+        }
+    }
+
+    private fun limpiarCampos(vararg campos: EditText) {
+        campos.forEach { it.text.clear() }
     }
 }
