a0753933a7eec4e70839b1b1c8aa62cc80034d45
Diego Alba <dayo.alba.p@gmail.com>
2024-12-17 20:51:12 -0400

    Room in develop

--- app/src/main/java/com/example/padresdinamicos/dataclasses/Category.kt
+++ app/src/main/java/com/example/padresdinamicos/dataclasses/Category.kt
@@ -1,11 +1,6 @@
 package com.example.padresdinamicos.dataclasses
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity
 data class Category(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val name: String,
     val subcategories: List<Subcategory>
 )
