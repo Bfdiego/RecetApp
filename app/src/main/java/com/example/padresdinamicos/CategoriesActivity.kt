2da3ef3556b9d35bcff73c1c6d72c524b7bb3c45
JorgiJust <saenz.jorge.inigues@gmail.com>
2024-12-18 01:39:24 -0400

    User Screen now works, buttons added to menu and categories now it can lead to the profile screen

--- app/src/main/java/com/example/padresdinamicos/CategoriesActivity.kt
+++ app/src/main/java/com/example/padresdinamicos/CategoriesActivity.kt
@@ -2,16 +2,19 @@ package com.example.padresdinamicos
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.padresdinamicos.databinding.ActivityCategoriesBinding
 import com.example.padresdinamicos.databinding.ActivityMenuBinding
+import com.example.padresdinamicos.databinding.ActivityUserScreenBinding
 
 class CategoriesActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCategoriesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityCategoriesBinding.inflate(layoutInflater)
         enableEdgeToEdge()
@@ -21,6 +24,11 @@ class CategoriesActivity : AppCompatActivity() {
         binding.menuIcon.setOnClickListener {
             val intentMenuActivity = Intent(this, MenuActivity::class.java)
             startActivity(intentMenuActivity)
+        }
+        binding.userIcon.setOnClickListener{
+            val intentUserActivity = Intent(this, userScreen::class.java)
+            startActivity(intentUserActivity)
+
         }
     }
 }
